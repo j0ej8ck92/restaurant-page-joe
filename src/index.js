23d17b5b745ac3cb14433f36257ced6b27d953da
j0ej8ck92 <j0ej8ck92@gmail.com>
2026-06-03 18:32:52 +0000

    DOM test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+//Home Page 4 Restaurant
+
+
 import "./styles.css";
 import { greeting } from "./module-1.js";
 
 console.log(greeting);
 
+function homePage(){
 
-//Home Page 4 Restaurant
+ const restaurantTitle = document.querySelector(".restaurant-title");
+ console.log(restaurantTitle);
+
+ const titleOfRestaurant = document.createElement("div");
+
+ console.log(titleOfRestaurant); //just some testing
+
+
+}
+
+homePage();
+
+
+
 
